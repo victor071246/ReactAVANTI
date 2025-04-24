9094f98d08b976e7d3a31312748cd93f32e9db19
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2025-04-23 23:07:13 -0300

    Add files via upload

---
+++ 4_themecontextapi/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1>Context api</h1>
+        </>
+    );
+}
+
+export default App;
---
+++ 4_themecontextapi/src/components/ThemeContext.jsx
@@ -0,0 +1,12 @@
+import React, {createContext} from 'react'
+import { useState } from 'react'
+
+export const ThemeContext = ({children}) => {
+    const [theme, setTheme] = useState("light")
+
+    const toggleTheme = () =>{}
+    
+    return (
+    )
+}
+
